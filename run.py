4626b56a0312351fb7e868fa79259662f2d6f2e6
OlgaKuvs <olja_kuv@inbox.lv>
2023-05-08 18:30:43 +0100

    add function to get stock values

--- run.py
+++ run.py
@@ -136,8 +136,14 @@ def calculate_stock_data(data):
 
     return new_stock_data
 
-
-
+def get_stock_values(data):    
+    headings  = SHEET.worksheet("stock").get_all_values()[0]    
+    dict = {}
+    count = 0 
+    for i in data:               
+        dict[headings[count]] = i        
+        count = count + 1          
+    return dict
 
 def main():
     """
@@ -150,10 +156,13 @@ def main():
     update_worksheet(new_surplus_data, "surplus")
     sales_columns = get_last_5_entries_sales()
     stock_data = calculate_stock_data(sales_columns)
-    update_worksheet(stock_data, "stock")  
-    
+    update_worksheet(stock_data, "stock") 
+    stock_values = get_stock_values(stock_data)    
+    print(f"Make the following numbers of sandwiches for next market: \n {stock_values}")
 
 
 print("Welcome to Love Sandwiches Data Automation")
 main()
+
+
 
